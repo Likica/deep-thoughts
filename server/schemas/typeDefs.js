c788ed70c50dbc713eedc29f768249910ce17bb2
Coco Puff <fahrija_wyroski@live.com>
2021-07-15 13:51:47 -0500

    added verification

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,13 +13,6 @@ type User {
     thoughts: [Thought]
     friends: [User]
   }
-
-  type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
-  }
   
   type Thought {
     _id: ID
@@ -37,6 +30,26 @@ type User {
     username: String
   }
 
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 
 `;
 
